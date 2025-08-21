2d90e57ca4b6d04f597d3e6e645542e1c30f02b5
Lucas-NLima <lucas.nhoquelima@gmail.com>
2025-08-21 15:26:59 -0300

    atualização 1.2

--- interestelar/missao1.php
+++ interestelar/missao1.php
@@ -30,14 +30,7 @@
 <p>1 -Sua nave acabou de aterrissar em um planeta desconhecido. Para explorar este novo mundo, você precisa desvendar o enigmas deixados pelos antigos habitantes. Cada resposta correta abrirá portas e revelará segredos do cosmos. Está pronto para o desafio?”</p>
 <span class="badge"><button class="neon">Responder</button></span>
 </main>
-=======
-    <h1>Informe um número par para liberar a informação sobre o planeta mais próximo do Sol (Mercúrio).</h1>
-    <form method="post">
-        <label for=""></label>
-    </form>
 
 
-    <a href="missao2.php"> <button>Proximo</button></a>
->>>>>>> 678c71f4a36505d3f29ee510c06300838e162eea
 </body>
 </html>
