1fb9828bb57068fa3a73a1245ee436cae8c61cd5
muPelegrin <pelegrinmurilo71@gmail.com>
2025-08-25 11:05:40 -0300

    refactor: update mission functionality and improve user input handling

--- interestelar/missao1.php
+++ interestelar/missao1.php
@@ -18,8 +18,26 @@
 
   <!-- seu conteúdo por cima do fundo -->
   <main class="conteudo">
-    <h1>Informe um número par para liberar a informação sobre o planeta mais próximo do Sol (Mercúrio)</h1>
+    <h1>Informe um número par para liberar a informação sobre o planeta mais próximo do Sol.</h1>
     <h4>Sua nave acabou de aterrissar em um planeta desconhecido. Para explorar este novo mundo, você precisa desvendar o enigmas deixados pelos antigos habitantes. Cada resposta correta abrirá portas e revelará segredos do cosmos. Está pronto para o desafio?.</h4>
   </main>
+   <form method="POST">
+        <input type="number" name="numero" required>
+        <button type="submit">Enviar</button>
+    </form>
 </body>
 </html>
+
+<?
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $numero = $_POST['numero'];
+
+    if ($numero % 2 == 0) {
+        echo "<h2>Mercúrio é o planeta mais próximo do Sol.</h2>";
+    }
+    else {
+        echo "<h2>Por favor, insira um número par.</h2>";
+    }
+}
+?>
